a246d11184323f38eee37f19c625885af931c294
Diego Stiehl <seccomiro@gmail.com>
2020-03-13 09:25:25 -0300

    Adiciona projeto Android

---
+++ app/src/main/java/br/edu/ifpr/stiehl/myapplication/MensagemActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifpr.stiehl.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MensagemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mensagem)
+    }
+}
